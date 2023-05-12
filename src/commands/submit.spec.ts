d52cbe6051009c90e198d47f67cae745ce542bb3
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2023-05-11 20:51:23 -0700

    fixup! refactor: remove single-step-upload now that the backend has support multistep for some time

--- src/commands/submit.spec.ts
+++ src/commands/submit.spec.ts
@@ -46,25 +46,4 @@ describe('submit()', () => {
       makeTestContext()
     );
   });
-
-  it('submits reports to check run reporter, falling back to single step upload', async () => {
-    nock('https://api.check-run-reporter.com')
-      .post('/api/v1/submissions/upload')
-      .reply(404);
-
-    nock('https://api.check-run-reporter.com')
-      .post('/api/v1/submissions')
-      .reply(201);
-
-    await submit(
-      {
-        label: 'foo',
-        report: ['reports/junit/**/*.xml'],
-        root: '/',
-        sha: '40923a72ddf9eefef938355fa96246607c706f6c',
-        token: 'FAKE TOKEN',
-      },
-      makeTestContext()
-    );
-  });
 });
